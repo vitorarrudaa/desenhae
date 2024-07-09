a5ad24f2fad918c556621181ce540e6a4b46aa3f
Joao Vitor <31460288+vitorarrudaa@users.noreply.github.com>
2024-07-09 08:33:31 -0400

    Implementada função de desenhar Apesar da funcção desenhar estar funcionando, muitos outros aspectos estão com problemas, como o botão limpar e o cor

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const clear_btn = document.querySelector("#clear_btn");
 const color_btn = document.querySelector("#color_btn");
 const size_btn = document.querySelector("#size_btn");
-const canvas = document.querySelector(".canvas");
+const screen = document.querySelector(".screen");
 
-let canvas_size;
+let screen_size;
+
+function screenGrid (screen_size){
+    for (let i=0; i<screen_size; i++){//creates a number of columns based on the input of the user
+        let col = document.createElement("div");
+        col.classList.add("screen_column");
+        for (let j=0; j<screen_size; j++){//creates a number of lines inside the columns
+            let block = document.createElement("div");
+            block.classList.add("screen_square");
+
+            colorBlock(block); //call a function to add an event listener to every block created
+            col.appendChild(block);
+        }
+        screen.appendChild(col);
+    }
+}
+
+function colorBlock (block){
+    block.addEventListener ("mouseenter", () => {
+        block.style.background = ("yellow");
+    })
+}
 
 size_btn.addEventListener ("click", () => {
-    canvas_size = parseInt(prompt("Qual o tamanho desejado?"));
-    alert (canvas_size);
+    screen_size = parseInt(prompt("Qual o tamanho desejado?"));
+    screenGrid(screen_size);
 });
+
+clear_btn.addEventListener ("click", () =>{
+    
+})
+
 
